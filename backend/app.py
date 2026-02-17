d0453a0a691a8a02a2ac574b1d6bf1e4949f461b
Nishi Jain <nishi.2410@gmail.com>
2026-02-17 14:32:00 +0530

    Added Docker build workflow for PROD

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,24 @@
-print("Feature branch execution - version 3")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+app = FastAPI()
+
+class Message(BaseModel):
+    name: str
+    message: str
+
+@app.get("/")
+def read_root():
+    return {
+        "status": "Application Running",
+        "timestamp": datetime.now()
+    }
+
+@app.post("/send")
+def send_message(data: Message):
+    return {
+        "received_from": data.name,
+        "message": data.message,
+        "processed_at": datetime.now()
+    }
